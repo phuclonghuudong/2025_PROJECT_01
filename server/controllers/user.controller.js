4b6d80a45aef92e2cf8d9c84d4ec2aa429031a55
phuclonghuudong <phucpham.241099@gmail.com>
2025-01-13 10:05:24 +0700

    add func logout, update avatar, add extension multer, cloudinary

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 import generateAccessToken from "../utils/generateAccessToken.js";
 import generateRefreshToken from "../utils/generateRefreshToken.js";
+import uploadImageCloudinary from "../utils/uploadImageCloudinary.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -159,6 +160,67 @@ export async function loginController(request, response) {
       data: {
         accessToken,
         refreshToken,
+      },
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function logoutController(request, response) {
+  try {
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    response.clearCookie("accessToken", cookiesOption);
+    response.clearCookie("refreshToken", cookiesOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(
+      request?.userId,
+      {
+        refresh_token: "",
+      }
+    );
+
+    return response.json({
+      message: "Logout successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function uploadAvatar(request, response) {
+  try {
+    const userId = request?.userId;
+    const image = request?.file;
+
+    const upload = await uploadImageCloudinary(image);
+
+    const updateUser = await UserModel.findByIdAndUpdate(userId, {
+      avatar: upload.url,
+    });
+
+    return response.json({
+      message: "Upload avatar",
+      error: false,
+      success: true,
+      data: {
+        _id: userId,
+        avatar: upload.url,
       },
     });
   } catch (error) {
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import {
   loginController,
+  logoutController,
   registerUserController,
+  uploadAvatar,
   verifyEmailController,
 } from "../controllers/user.controller.js";
+import auth from "../middleware/auth.js";
+import upload from "../middleware/multer.js";
 
 const userRouter = Router();
 userRouter.post("/register", registerUserController);
 userRouter.post("/verify-email", verifyEmailController);
 userRouter.post("/login", loginController);
+userRouter.get("/logout", auth, logoutController);
+userRouter.put("/update-avatar", auth, upload.single("avatar"), uploadAvatar);
 
 export default userRouter;
